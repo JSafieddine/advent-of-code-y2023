e909b9b87c725de032f3bd9962eee199dfcb0b2f
Jamal Safieddine <mister.recursive@gmail.com>
2023-12-06 08:49:43 +0100

    Solved day 6.

--- src/AdventOfCode/Day6.hs
+++ src/AdventOfCode/Day6.hs
@@ -1,5 +1,12 @@
 module AdventOfCode.Day6 (day6, part1, part2) where
 
+import Data.Void (Void)
+import Text.Megaparsec (Parsec, eof, parse, some)
+import Text.Megaparsec.Char (eol, hspace, string)
+import Text.Megaparsec.Char.Lexer (decimal)
+
+type Parser = Parsec Void String
+
 day6 :: IO ()
 day6 = do
   input <- readFile "src/Data/Day6.txt"
@@ -9,7 +16,36 @@ day6 = do
   print . part2 $ input
 
 part1 :: String -> Int
-part1 _ = undefined
+part1 input = product . map waysToWin $ races
+  where
+    races = case parse inputParser "" input of
+      (Right result) -> result
+      (Left _) -> error "Can't parse input!"
 
 part2 :: String -> Int
-part2 _ = undefined
+part2 input = waysToWin . getRaceTime $ races
+  where
+    races = case parse inputParser "" input of
+      (Right result) -> result
+      (Left _) -> error "Can't parse input!"
+
+inputParser :: Parser [(Int, Int)]
+inputParser = do
+  _ <- string "Time:"
+  times <- some (hspace *> decimal)
+  _ <- eol
+  _ <- string "Distance:"
+  distance <- some (hspace *> decimal)
+  _ <- eol *> eol *> eof
+  return $ zip times distance
+
+waysToWin :: (Int, Int) -> Int
+waysToWin (t, d) = length . filter (recordBeat t d) $ [0 .. t]
+
+recordBeat :: Int -> Int -> Int -> Bool
+recordBeat t d b = b * (t - b) > d
+
+getRaceTime :: [(Int, Int)] -> (Int, Int)
+getRaceTime parts = (convert fst parts, convert snd parts)
+  where
+    convert part = read . concatMap (show . part)
--- test/AdventOfCode/Day6Spec.hs
+++ test/AdventOfCode/Day6Spec.hs
@@ -4,12 +4,12 @@ module AdventOfCode.Day6Spec (spec) where
 
 import AdventOfCode.Day6 (part1, part2)
 import Data.Functor ((<&>))
-import Test.Hspec (Spec, describe, it, pendingWith, shouldReturn)
+import Test.Hspec (Spec, describe, it, shouldReturn)
 
 spec :: Spec
 spec = do
   describe "test day 6" $ do
     it "part one" $
-      pendingWith "Not yet implemented!" <* ((readFile "test/Data/Day6.txt" <&> part1) `shouldReturn` -1)
+      (readFile "test/Data/Day6.txt" <&> part1) `shouldReturn` 288
     it "part two" $
-      pendingWith "Not yet implemented!" <* ((readFile "test/Data/Day6.txt" <&> part2) `shouldReturn` -1)
+      (readFile "test/Data/Day6.txt" <&> part2) `shouldReturn` 71503
