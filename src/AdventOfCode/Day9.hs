28036f6c355a093e79230b122690bd7fb3faea62
Jamal Safieddine <mister.recursive@gmail.com>
2023-12-09 06:40:43 +0100

    Solved day 9.

--- src/AdventOfCode/Day9.hs
+++ src/AdventOfCode/Day9.hs
@@ -9,7 +9,45 @@ day9 = do
   print . part2 $ input
 
 part1 :: String -> Int
-part1 _ = undefined
+part1 = sum . map extrapolateLastValue . inputReverseParser
 
 part2 :: String -> Int
-part2 _ = undefined
+part2 = sum . map extrapolateFirstValue . inputParser
+
+type Report = [Int]
+
+type ReverseReport = [Int]
+
+type DiffReverseReport = [Int]
+
+inputReverseParser :: String -> [ReverseReport]
+inputReverseParser = map (reverse . map read . words) . init . lines
+
+inputParser :: String -> [Report]
+inputParser = map (map read . words) . init . lines
+
+rdiff :: ReverseReport -> DiffReverseReport
+rdiff report = zipWith (-) report (tail report)
+
+rdiffs :: ReverseReport -> [DiffReverseReport]
+rdiffs report
+  | all (== 0) report = []
+  | otherwise = dReport : rdiffs dReport
+  where
+    dReport = rdiff report
+
+extrapolateLastValue :: ReverseReport -> Int
+extrapolateLastValue report = sum . map head $ (report : rdiffs report)
+
+diff :: Report -> Report
+diff report = zipWith (-) (tail report) report
+
+diffs :: Report -> [Report]
+diffs report
+  | all (== 0) report = []
+  | otherwise = dReport : diffs dReport
+  where
+    dReport = diff report
+
+extrapolateFirstValue :: Report -> Int
+extrapolateFirstValue report = foldr1 (-) . map head $ (report : diffs report)
--- test/AdventOfCode/Day9Spec.hs
+++ test/AdventOfCode/Day9Spec.hs
@@ -4,12 +4,12 @@ module AdventOfCode.Day9Spec (spec) where
 
 import AdventOfCode.Day9 (part1, part2)
 import Data.Functor ((<&>))
-import Test.Hspec (Spec, describe, it, pendingWith, shouldReturn)
+import Test.Hspec (Spec, describe, it, shouldReturn)
 
 spec :: Spec
 spec = do
   describe "test day 9" $ do
     it "part one" $
-      pendingWith "Not yet implemented!" <* ((readFile "test/Data/Day9.txt" <&> part1) `shouldReturn` -1)
+      (readFile "test/Data/Day9.txt" <&> part1) `shouldReturn` 114
     it "part two" $
-      pendingWith "Not yet implemented!" <* ((readFile "test/Data/Day9.txt" <&> part2) `shouldReturn` -1)
+      (readFile "test/Data/Day9.txt" <&> part2) `shouldReturn` 2
