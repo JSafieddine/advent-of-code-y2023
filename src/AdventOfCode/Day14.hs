cbf17afe380a76e063e28490a4963197170f26f4
Jamal Safieddine <mister.recursive@gmail.com>
2023-12-15 05:49:34 +0100

    Solved day 14.

--- src/AdventOfCode/Day14.hs
+++ src/AdventOfCode/Day14.hs
@@ -1,5 +1,10 @@
 module AdventOfCode.Day14 (day14, part1, part2) where
 
+import Control.Monad.State (MonadState (put), State, evalState, get)
+import Data.List (elemIndex, intercalate, sortBy, transpose)
+import Data.List.Split (splitOn)
+import Prelude hiding (cycle)
+
 day14 :: IO ()
 day14 = do
   input <- readFile "src/Data/Day14.txt"
@@ -9,7 +14,44 @@ day14 = do
   print . part2 $ input
 
 part1 :: String -> Int
-part1 _ = undefined
+part1 = calculcateLoad . tiltDirection North . inputParser
 
 part2 :: String -> Int
-part2 _ = undefined
+part2 input = calculcateLoad (iterate cycle loopCycle !! cyclesFromLoop)
+  where
+    rocks = inputParser input
+    (cycleEnd, cycleStart, loopCycle) = evalState findLoop [rocks]
+    cyclesFromLoop = (1000000000 - cycleStart) `mod` (cycleEnd - cycleStart)
+
+inputParser :: String -> [String]
+inputParser = init . lines
+
+data TiltDirection = North | West | South | East deriving (Show, Enum, Bounded)
+
+tiltDirection :: TiltDirection -> [String] -> [String]
+tiltDirection North = transpose . tilt . transpose
+tiltDirection West = tilt
+tiltDirection South = transpose . map reverse . tilt . map reverse . transpose
+tiltDirection East = map reverse . tilt . map reverse
+
+tilt :: [String] -> [String]
+tilt = map ((intercalate "#" . map (sortBy (flip compare))) . splitOn "#")
+
+calculcateLoad :: [String] -> Int
+calculcateLoad = sum . map calculcateLoadLine . transpose
+
+calculcateLoadLine :: String -> Int
+calculcateLoadLine = sum . map fst . filter ((== 'O') . snd) . zip [1 ..] . reverse
+
+cycle :: [String] -> [String]
+cycle rocks = foldl (flip tiltDirection) rocks [minBound .. maxBound]
+
+findLoop :: State [[String]] (Int, Int, [String])
+findLoop = do
+  prevCycles <- get
+  let nextCycle = cycle . head $ prevCycles
+  case nextCycle `elemIndex` prevCycles of
+    (Just n) -> return (length prevCycles, length prevCycles - n - 1, nextCycle)
+    Nothing -> do
+      put (nextCycle : prevCycles)
+      findLoop
--- test/AdventOfCode/Day14Spec.hs
+++ test/AdventOfCode/Day14Spec.hs
@@ -4,12 +4,12 @@ module AdventOfCode.Day14Spec (spec) where
 
 import AdventOfCode.Day14 (part1, part2)
 import Data.Functor ((<&>))
-import Test.Hspec (Spec, describe, it, pendingWith, shouldReturn)
+import Test.Hspec (Spec, describe, it, shouldReturn)
 
 spec :: Spec
 spec = do
   describe "test day 14" $ do
     it "part one" $
-      pendingWith "Not yet implemented!" <* ((readFile "test/Data/Day14.txt" <&> part1) `shouldReturn` -1)
+      (readFile "test/Data/Day14.txt" <&> part1) `shouldReturn` 136
     it "part two" $
-      pendingWith "Not yet implemented!" <* ((readFile "test/Data/Day14.txt" <&> part2) `shouldReturn` -1)
+      (readFile "test/Data/Day14.txt" <&> part2) `shouldReturn` 64
